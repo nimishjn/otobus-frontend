eaea6845c3a44b380badd70c23c85c3d68a13de3
agtarunika <tarunika1607@gmail.com>
2021-11-07 19:38:53 +0530

    feat: adding delete booking

--- app/components/listBooking/listBooking.js
+++ app/components/listBooking/listBooking.js
@@ -6,4 +6,9 @@ listBookVar.controller('ListBookingCtrl', function ($scope, $http) {
   $http.get("./assets/booking.json").then(function (response) {
     $scope.bookings = response.data;
   });
+
+  $scope.deleteBooking=function(id){
+    console.log("deleted " +id);
+    $scope.myData.splice($scope.myData.indexOf(id))
+  }
 });
