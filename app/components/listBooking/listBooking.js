420d0c009d0ee38284a25c93f05f6b22778de429
agtarunika <tarunika1607@gmail.com>
2021-11-08 00:08:47 +0530

    fix delete booking

--- app/components/listBooking/listBooking.js
+++ app/components/listBooking/listBooking.js
@@ -1,14 +1,19 @@
 "use strict";
 
 var listBookVar = angular.module("myApp.listBooking", []);
-
+var deleteDataId="";
 listBookVar.controller('ListBookingCtrl', function ($scope, $http) {
   $http.get("./assets/booking.json").then(function (response) {
     $scope.bookings = response.data;
   });
 
-  $scope.deleteBooking=function(id){
-    console.log("deleted " +id);
-    $scope.myData.splice($scope.myData.indexOf(id))
+  $scope.deleteData=function(deleteBusId){
+    deleteDataId=deleteBusId;
+    console.log(deleteDataId);
+  }
+
+  $scope.deleteBooking=function(){
+    console.log("deleted " + deleteDataId);
+    deleteDataId="";
   }
 });
