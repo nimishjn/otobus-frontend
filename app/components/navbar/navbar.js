182f140f3bca4e1e96989c92e065cf029968ca5a
nimishjn <nimishjain100701@gmail.com>
2021-11-08 17:55:06 +0530

    fix: Navbar fix and UI fix

--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -1,9 +1,16 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('nav_links')[0];
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('nav_active');
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("nav_links")[0];
+
+toggleButton.addEventListener("click", () => {
+  toggleNavbar();
 });
+
 const navbarClose = document.getElementById("close-navbar");
-navbarClose.addEventListener('click', () => {
-    navbarLinks.classList.toggle('nav_active');
-})
+
+navbarClose.addEventListener("click", () => {
+  toggleNavbar();
+});
+
+function toggleNavbar() {
+  navbarLinks.classList.toggle("nav_active");
+}
