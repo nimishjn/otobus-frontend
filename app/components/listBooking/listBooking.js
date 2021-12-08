5c04e7638a52ce373d9521acfc03cca899735c3c
nimishjn <nimishjain100701@gmail.com>
2021-12-08 17:25:23 +0530

    feat: list Bookings route integration

--- app/components/listBooking/listBooking.js
+++ app/components/listBooking/listBooking.js
@@ -1,19 +1,33 @@
 "use strict";
 
 var listBookVar = angular.module("myApp.listBooking", []);
-var deleteDataId="";
-listBookVar.controller('ListBookingCtrl', function ($scope, $http) {
-  $http.get("./assets/booking.json").then(function (response) {
-    $scope.bookings = response.data;
-  });
+var deleteDataId = "";
+listBookVar.controller("ListBookingCtrl", function ($scope, $http) {
+  $http({
+    url: "http://localhost:3001/listBookings",
+    method: "get",
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  })
+    .then(function (response) {
+      if (response.data.code === "S2") {
+        $scope.bookings = response.data.bookings;
+      }
+    })
+    .catch(function (err) {
+      alert(
+        "Please login to continue. If you are logged in, then log out and login again."
+      );
+    });
 
-  $scope.deleteData=function(deleteBusId){
-    deleteDataId=deleteBusId;
+  $scope.deleteData = function (deleteBusId) {
+    deleteDataId = deleteBusId;
     console.log(deleteDataId);
-  }
+  };
 
-  $scope.deleteBooking=function(){
+  $scope.deleteBooking = function () {
     console.log("deleted " + deleteDataId);
-    deleteDataId="";
-  }
+    deleteDataId = "";
+  };
 });
